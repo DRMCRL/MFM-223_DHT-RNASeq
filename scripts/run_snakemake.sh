2f025f775af92b71e1aab330462889b5e74a7e5e
Steve Pederson <stephen.pederson@adelaide.edu.au>
2020-10-14 11:24:10 +1030

    Reset for run with correct sjdbOverhang

--- scripts/run_snakemake.sh
+++ scripts/run_snakemake.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #SBATCH -p batch
 #SBATCH -N 1
-#SBATCH -n 4
-#SBATCH --time=1:00:00
-#SBATCH --mem=32GB
-#SBATCH -o /home/a1018048/slurm/20131906_MDA-MB-453_DHT-RNASeq/%x_%j.out
-#SBATCH -e /home/a1018048/slurm/20131906_MDA-MB-453_DHT-RNASeq/%x_%j.err
+#SBATCH -n 16
+#SBATCH --time=4:00:00
+#SBATCH --mem=128GB
+#SBATCH -o /home/a1018048/slurm/MFM-223_DHT-RNASeq/%x_%j.out
+#SBATCH -e /home/a1018048/slurm/MFM-223_DHT-RNASeq/%x_%j.err
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=stephen.pederson@adelaide.edu.au
 
 ## Cores
-CORES=8
+CORES=32
 if [ -d "/hpcfs" ]; then
 	module load arch/arch/haswell
 	module load arch/haswell
@@ -26,7 +26,7 @@ else
 fi
 
 ## Project Root
-PROJ=${HPC}/users/a1018048/20131906_MDA-MB-453_DHT-RNASeq
+PROJ=${HPC}/users/a1018048/MFM-2233_DHT-RNASeq
 
 ## The environment containing snakemake
 micromamba activate snakemake
